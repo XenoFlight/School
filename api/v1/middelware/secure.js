b73a6f3c45bd26b73405e0e3aa2a2fcbf5ec4143
פבל - פקח ובקר רישוי עסקים <[altz.xenowolf@gmail.com]>
2025-01-07 18:58:14 +0200

    this is my shit

--- api/v1/middelware/secure.js
+++ api/v1/middelware/secure.js
@@ -1,6 +1,6 @@
 module.exports= (req,res,next)=>{
     let arrIps=[];
-    for (let i=0;i<arrIps.length;++)
+    for (let i=0;i<arrIps.length;i++)
     {
         console.log(req.ip);
         if (req.ip==arrIps[i])
